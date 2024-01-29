0c4fe57c260d03413d002f15d510d92339d90c6b
Neil1015 <139834032+Neil1015@users.noreply.github.com>
2024-01-28 16:50:49 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name = "Neil";
+var last_name = "Kapoor";
+var a = 10;
+var b = 5;
+var full_name = full_name.concat(last_name);
+console.log(full_name);
+var add = a + b;
+console.log(add);
